2e998ac95ba8de72ac6b53a95c14ac0e12629e8d
Wojciech Wluka <wojtek.wluka@onet.pl>
2022-03-15 21:24:40 +0100

    Koniec

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
-import pygame
-import sys
-import time
-from object import pilka
-from object import paletka
+import pygame #Zaimportowanie biblioteki Pygame
+import sys #Zaimportowanie biblioteki systemowej
+import time #Zaimportowanie bilbioteki czasu
+from object import pilka #Zaimportowanie klasy pilka z pliku object.py
+from object import paletka #Zaimportowanie klasy paletka z pliku object.py
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": #Jeśli program jest programem głównym
     class Game(object):
-        def __init__(self):
-            self.clock = pygame.time.Clock()
-            icon = pygame.image.load("pong.png")
-            self.screen_image = pygame.image.load("nature-2384_960_720.jpg")
-            pygame.init()
-            self.resolution = [700,600]
-            self.display = pygame.display.set_mode(self.resolution)
-            pygame.display.set_caption("Taki tam Pong")
-            pygame.display.set_icon(icon)
-            self.dt = 0.0
-            self.prev_time = time.time()
-            self.MAX_FPS = 144
+        def __init__(self): #Stworzenie fukcji inicjalizującej
+            self.clock = pygame.time.Clock() #Utworzenie obiektu zegara
+            icon = pygame.image.load("pong.png") #Zaimportowanie ikony okienka
+            self.screen_image = pygame.image.load("nature-2384_960_720.jpg") #Zaimportowanie tła okna
+            pygame.init() #Zainicjalizowanie biblioteki
+            self.resolution = [700,600] #Utworzenie listy z rozdzielczością obrazu
+            self.display = pygame.display.set_mode(self.resolution) #Utworznie obiektu ekranu
+            pygame.display.set_caption("Taki tam Pong") #Nadanie tytułu dla okna
+            pygame.display.set_icon(icon) #Ustawienie ikony
+            self.dt = 0.0 #Utworzenie zmiennej delta time
+            self.prev_time = time.time() #Pobranie czasu obecnego
+            self.MAX_FPS = 144 #Ustawienie maksymalnej liczby FPS dla lepszej wydajności na komputerach o niższej mocy obliczeniowej
 
+            #Zdefiniowanie pozycji początkowej dla obiektów piłki i graczy pos_x1 i pos_y1 dla gracza 1; pos_x2 i pos_y2 dla gracza 2 i ball_x oraz ball_y dla piłki
             self.pos_x1 = 10
             self.pos_y1 = self.resolution[1] / 2 - 90
             self.pos_x2 = self.resolution[0] - 25 - 10
@@ -28,49 +29,72 @@ if __name__ == "__main__":
             self.ball_x = self.resolution[0] / 2 - 10
             self.ball_y = self.resolution[1] / 2  - 10
 
+            #Ustawienie prędkości poruszania się obiektów player_speed dla graczy i x_speed oraz y_speed dla piłki
+
             self.player_speed = 5
             self.x_speed = 1
             self.y_speed = 1
 
+            #Utworzenie obiektów piłki i graczy oraz przekazanie im argumentu self, czyli klasa bala otrzyma dostęp do wszystkich zmiennych klasy Game tak samo dla klasy palatka. Obie klasy zamieszczone są w pliku object.py
+
             self.bala = pilka(self)
             self.players = paletka(self)
 
-            self.paused = False
+            self.paused = False #Ustawienie początkowego stany gry, Pauza gry ustawiona na Fałsz
+
+            #Import czcionek do wyświetlenia na ekranie podczas końca gry
             self.font = pygame.font.SysFont(None,50)
-            while True:
-                    if self.paused == True:
-                        for ev in pygame.event.get():
-                            if ev.type == pygame.QUIT:
+            self.font2 = pygame.font.SysFont(None,30)
+
+            while True: #Zapętlenie wykonywania programu
+                    if self.paused == True: #Jeśli gra się zakończyła
+                        for ev in pygame.event.get(): #Pętla która sprawdza czy system wykryje jakiś event z okna
+                            if ev.type == pygame.QUIT: #Jeśli zostanie naciśnięty "zamnij okno" to zamyka gre
+                                pygame.QUIT
                                 sys.exit()
-                            keys = pygame.key.get_pressed()
-                            if keys[pygame.K_f]:
-                                game = Game()
-                        self.msg(f"{self.bala.win} win the game", (132,45,21))
-                        pygame.display.flip()
-                    if self.paused == False:
-                        self.clock.tick(100)
-                        self.now = time.time()
-                        self.dt = self.prev_time - self.now
-                        self.prev_time = self.now
-                        print(self.dt)
+                            keys = pygame.key.get_pressed() #pobranie wszystkich dostępnych klawiszy
+                            if keys[pygame.K_c]: #Jeśli klawisz c naciśnięty
+                                game = Game() #Uruchomi grę ponownie
+                            if keys[pygame.K_ESCAPE]: #Jeśli klawisz ESC naciśnięty zamyka grę i okno
+                                pygame.QUIT
+                                sys.exit()
+
+                        self.msg(f"{self.bala.win} win the game", (132,45,21)) #Wyświetlenie wiadomości w kolorze, kto wygrał gre
+                        self.msg2("Press ESC to QUIT or c to continue the game", (132,45,21)) #Wyświetlenie wiadomości co użytkownik może zrobić dalej
+                        pygame.display.flip() #Odświerzenie ekranu
+                    if self.paused == False: #Jeśli gra trwa
+                        self.clock.tick(100) #Ustawienie ilości FPS komputera na 100
+                        self.now = time.time() #Pobranie aktualnego czasu
+                        self.dt = self.prev_time - self.now #Zmiana wartości delta time. Delta time to różnica czasu początku jednej klatki do początku drugiej klatki
+                        self.prev_time = self.now #Teraz początkiem klatki będzie now i tak do końca trwania gry
+                        #Delta time pomaga zapobiec opóźnieniom w przypadku komputerów o niższej mocy obliczeniowej. Np. Komputer o wydajności 60 FPS będzie miał taką samą prędkość jak komputer z 30FPS. Kosztem jest czas im więcej FPS tym czas krótszy.
+                        #Gdyby delta time nie zostało zaimplemetowanie a gra miałaby być onnline to gracze o niższej mocy obliczeniowej komputera nie mogli by wygrać rozgrywki
+
+
+                        #Zmiana prędkości obiektu piłki z uwgdlędnieniem maksymalnej liczby FPS oraz wcześniej wspomnianego delta time
                         self.y_speed += self.y_speed * self.dt * self.MAX_FPS
                         self.x_speed += self.x_speed * self.dt * self.MAX_FPS
-                        for ev in pygame.event.get():
-                            if ev.type == pygame.QUIT:
+
+                        for ev in pygame.event.get(): #Pętla która pobiera eventy z biblioteki
+                            if ev.type == pygame.QUIT: #Jeśli naciśnięto "zamknij okno" to gra się zamyka
+                                pygame.QUIT
                                 sys.exit()
-                        self.move()
+                        self.move() #Wykonanie fukcji poruszania obiektami. Funkcja napisana niżej
 
-                        self.display.blit(self.screen_image, (0, 0))
-                        self.draw()
-        def draw(self):
-            self.bala.draw()
-            self.players.draw()
-            pygame.display.flip()
-        def move(self):
-            self.bala.move()
-            self.players.move()
-        def msg(self,msgs,color):
-            self.screen_text = self.font.render(msgs, True,color)
-            self.display.blit(self.screen_text, [self.resolution[0] / 2 - 160, self.resolution[1] / 2 - 10])
+                        self.display.blit(self.screen_image, (0, 0)) #Narysowanie ekranu
+                        self.draw() #Wykonanie funkcji rysowania obiektów. Funkcja napisana niżej
+        def draw(self): #Funckja ogólna do rysowania
+            self.bala.draw() #wykonanie funckji rysowania obiektu piłki. Funckja znajduje się w klasie pilka w pliku object.py
+            self.players.draw() #wykonanie funkcji rysowania obiektu graczy. Funkcja znajduje się w klasie paletka w pliku object.py
+            pygame.display.flip() #Odświerzenie ekranu
+        def move(self): #Funckja poruszania
+            self.bala.move() #wykonanie funckji poruszania obiektu piłki. Funckja znajduje się w klasie pilka w pliku object.py
+            self.players.move()#wykonanie funkcji poruszania obiektu graczy. Funkcja znajduje się w klasie paletka w pliku object.py
+        def msg(self,msgs,color): #Funcja odpowiadająca za wiadomośc kto wygrał
+            self.screen_text = self.font.render(msgs, True,color) #Pobiera wiadomość do zmiennej
+            self.display.blit(self.screen_text, [self.resolution[0] / 2 - 160, self.resolution[1] / 2 - 10]) #Wyświetla na ekranie text - pierwszy argument w określonym miejscu ekranu - drugi argument
+        def msg2(self,msgs,color): #Funcja odpowiadająca za wiadomośc o dalszych możliwościach programy
+            self.screen_text = self.font2.render(msgs, True,color) #Pobiera wiadomość do zmiennej
+            self.display.blit(self.screen_text, [self.resolution[0] / 2 - 200, self.resolution[1] / 2 + 100])  #Wyświetla na ekranie text - pierwszy argument w określonym miejscu ekranu - drugi argument
 
-game = Game()
+game = Game() #Utworzenie obiektu dla klasy Game
--- object.py
+++ object.py
@@ -1,39 +1,48 @@
 import pygame
-class paletka():
-    def __init__(self,game):
-        self.game = game
+class paletka(): #Utworznie klasy dla graczy
+    def __init__(self,game): #Zainicjalizowanie klasy oraz wymagany argument game czyli cała klasa Game
+        self.game = game #Przypisanie do klasy wszyskich zmiennych klasy Game do zmiennej game
+
+        #Pobieranie informacji z klasy Game o ekranie
         self.display = self.game.display
         self.pos1_x = self.game.pos_x1
         self.pos1_y = self.game.pos_y1
         self.pos2_x = self.game.pos_x2
         self.pos2_y = self.game.pos_y2
+
+        #Utworznie obiektu prostokąta dla obu graczy
         self.player1 = pygame.Rect(self.pos1_x,self.pos1_y,25,150)
         self.player2 = pygame.Rect(self.pos2_x, self.pos2_y, 25, 150)
 
-    def move(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP]:
-            self.player1.y -= self.game.player_speed
-        if keys[pygame.K_DOWN]:
-            self.player1.y += self.game.player_speed
-        if keys[pygame.K_w]:
-            self.player2.y -= self.game.player_speed
-        if keys[pygame.K_s]:
-            self.player2.y += self.game.player_speed
-        if self.player1.top <= 0:
-            self.player1.y += 10
-        if self.player1.bottom >= self.game.resolution[1]:
-            self.player1.y -= 10
-        if self.player2.top <= 0:
-            self.player2.y += 10
-        if self.player2.bottom >= self.game.resolution[1]:
-            self.player2.y -= 10
+    def move(self): #Funkcja poruszania
+        keys = pygame.key.get_pressed() #Pobranie wszystkich klawiszy
+        if keys[pygame.K_UP]: #Jeśli klawisz w górę wciśnięty
+            self.player1.y -= self.game.player_speed #Odejmij od pozycji gracza1 prędkość gracza
+        if keys[pygame.K_DOWN]: #Jeśli klawisz w dół wciśnięty
+            self.player1.y += self.game.player_speed #Dodaj do pozycji gracza1 prędkość gracza
+        if keys[pygame.K_w]:  #Jeśli klawisz w  wciśnięty
+            self.player2.y -= self.game.player_speed  #Odejmij od pozycji gracza2 prędkość gracza
+        if keys[pygame.K_s]:  #Jeśli klawisz s wciśnięty
+            self.player2.y += self.game.player_speed  #Dodaj do pozycji gracza2 prędkość gracza
+
+        #Kolizje paletek
+        if self.player1.top <= 0: #Jeśli góra gracza nr.1 jest mniejsza lub równa 0 - góra ekranu
+            self.player1.y += 10 #Zmiena pozycje gracza1 żeby nie mógł dalej poruszać się w górę
+        if self.player1.bottom >= self.game.resolution[1]: #Jeśli dół gracza nr.1 jest większy lub równy maksymalnej rozdzielczości
+            self.player1.y -= 10  #Zmiena pozycje gracza1 żeby nie mógł dalej poruszać się w dół
+
+        if self.player2.top <= 0: #Jeśli góra gracza nr.2 jest mniejsza lub równa 0 - góra ekranu
+            self.player2.y += 10 #Zmiena pozycje gracza2 żeby nie mógł dalej poruszać się w górę
+        if self.player2.bottom >= self.game.resolution[1]:  #Jeśli dół gracza nr.2 jest większy lub równy maksymalnej rozdzielczości
+            self.player2.y -= 10  #Zmiena pozycje gracza2 żeby nie mógł dalej poruszać się w dół
 
 
-    def draw(self):
-        pygame.draw.rect(self.display, (0, 150, 255), self.player1)
-        pygame.draw.rect(self.display, (0, 150, 255), self.player2)
-class pilka():
+    def draw(self): #Rysowanie na ekranie graczy
+        pygame.draw.rect(self.display, (0, 150, 255), self.player1) #Narysowanie obiektu gracza nr.1
+        pygame.draw.rect(self.display, (0, 150, 255), self.player2) #Narywanie obiektu gracza nr.2
+
+class pilka(): #Utworzenie klasy piłki
+    #Funcja __init__ działa na takiej samej zasadzie jak funckja __init__ klasy paletka
     def __init__(self,game):
         self.game = game
         self.display = self.game.display
@@ -43,23 +52,33 @@ class pilka():
         self.speed_x = self.game.x_speed
         self.speed_y = self.game.y_speed
 
-    def move(self):
+    def move(self): #Funkcja poruszania piłki
+        #Domyślnie ustanione że piłka będzie poruszać się w lewą górną cześć ekranu
         self.ball.x -= self.speed_x
         self.ball.y -= self.speed_y
 
-        if self.ball.colliderect(self.game.players.player1):
-            if abs(self.ball.left - self.game.players.player1.right) > 10:
+        #Kolizje piłki z graczem nr.1
+        if self.ball.colliderect(self.game.players.player1): #Funckja colliderect sprawdza czy obiekt ma kolizję z obiektem gracza nr.1. Jeśli tak to:
+            if abs(self.ball.left - self.game.players.player1.right) > 10: #Jeśli różnica odległości między lewą częścią piłki a prawą częścią gracza nr.1 jest większa od 10.
+                #Liczba 10 jest domyślnym standardem przyjmowanych różnicy odlegości w takich typu grach
+
+                #Po wykryciu tego typu zderzenia zmienia kierunek ruchu piłki
                 self.speed_y *= -1
                 self.speed_x *= -1
-            if abs(self.ball.right - self.game.players.player1.left) > 10:
+            if abs(self.ball.right - self.game.players.player1.left) > 10:  #Jeśli różnica odległości między prawą częścią piłki a lewą częścią gracza nr.1 jest większa od 10
+                # Po wykryciu tego typu zderzenia zmienia kierunek ruchu piłki
                 self.speed_y *= -1
                 self.speed_x *= -1
-            if abs(self.ball.bottom - self.game.players.player1.top) > 10:
+            if abs(self.ball.bottom - self.game.players.player1.top) > 10:  #Jeśli różnica odległości między dolną częścią piłki a górną częścią gracza nr.1 jest większa od 10
+                # Po wykryciu tego typu zderzenia zmienia kierunek ruchu piłki
                 self.speed_y *= -1
                 self.speed_x *= -1
-            if abs(self.ball.top - self.game.players.player1.bottom) > 10:
+            if abs(self.ball.top - self.game.players.player1.bottom) > 10: #Jeśli różnica odległości między górną częścią piłki a dolną częścią gracza nr.1 jest większa od 10
+                # Po wykryciu tego typu zderzenia zmienia kierunek ruchu piłki
                 self.speed_y *= -1
                 self.speed_x *= -1
+
+        #Kolizje piłki z graczem nr.2 działają na tej samej zasadzie co kolizje piłki z graczem nr. 1 jak powyżej
         if self.ball.colliderect(self.game.players.player2):
             if abs(self.ball.left - self.game.players.player2.right) > 10:
                 self.speed_y *= -1
@@ -74,15 +93,15 @@ class pilka():
                 self.speed_y *= -1
                 self.speed_x *= -1
 
-
-        if self.ball.top <= 0 or self.ball.bottom >= self.game.resolution[1]:
-            self.speed_y *= -1
-        if self.ball.left <= 0:
-            self.win = "Player2"
-            self.game.paused = True
-        if self.ball.right >= self.game.resolution[0]:
-            self.win = "Player1"
-            self.game.paused = True
-    def draw(self):
-        pygame.draw.ellipse(self.display, (125,50,75), self.ball)
+        #Kolizje piłki z ekranem
+        if self.ball.top <= 0 or self.ball.bottom >= self.game.resolution[1]: #Jeśli góra lub dół piłki dotykają dolnej lub górnej części ekranu
+            self.speed_y *= -1 #Pomnóż prędkość y piłki o -1 co zmienia kierunek ruchu piłki
+        if self.ball.left <= 0: #Jeśli lewa część piłki dotyka lewej krawędzi ekranu
+            self.win = "Player2" #Przekaż że wygrał gracz nr.2
+            self.game.paused = True #Ustaw że gra jest przerwana. Główny plik z grą
+        if self.ball.right >= self.game.resolution[0]: #Jeśli prawa część piłki dotyka prawej krawędzi ekrany
+            self.win = "Player1" #Przekaż że wygrał gracz nr.1
+            self.game.paused = True #Przerwij gre jak wyżej wspomniane
+    def draw(self): #Rysowanie piłki
+        pygame.draw.ellipse(self.display, (125,50,75), self.ball) #Wywołanie funkcji pygame.draw.ellipse która narysuje okrągłą piłkę
 
